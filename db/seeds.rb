f8a63bc6e2d4f91725dcbcf872ab53a5e78a6bc3
Amelie CARO <amelie.caro@orange.fr>
2019-08-07 14:36:32 +0200

    Schema Models Review.rb et Restaurant.rb OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,49 @@
 #
 # Examples:
 #
+
+Restaurant.destroy_all
+
+Restaurant.create!(
+  name: 'Le Globe',
+  stars: 3,
+  address: 'Quimper',
+  categorie: 'french'
+)
+
+Restaurant.create!(
+  name: 'Le Boeuf sur le Quai',
+  stars: 4,
+  address: 'Brest',
+  categorie: 'french'
+)
+
+Restaurant.create!(
+  name: 'La Frégate',
+  stars: 5,
+  address: 'Le Faou',
+  categorie: 'french'
+)
+
+Restaurant.create!(
+  name: 'Le Paul Germain',
+  stars: 5,
+  address: 'Brest',
+  categorie:'french'
+)
+
+Restaurant.create!(
+  name: 'Ty Lae',
+  stars: 2,
+  address: 'Chateauneuf du Faou',
+  categorie:'french'
+)
+
+puts 'Restaurants created!'
+
+
+
+
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
